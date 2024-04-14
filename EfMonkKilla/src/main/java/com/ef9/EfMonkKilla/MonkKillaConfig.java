01a50d5a006b359f2583d9d8126ab1a103711ca2
ef-9 <bin+ef9@headchef.co>
2024-04-14 13:36:59 +1200

    Ported the plugin and tested it runs/kills monks

---
+++ EfMonkKilla/src/main/java/com/ef9/EfMonkKilla/MonkKillaConfig.java
@@ -0,0 +1,39 @@
+package com.ef9.EfMonkKilla;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
+
+@ConfigGroup("MonkKilla")
+public interface MonkKillaConfig extends Config {
+    @ConfigItem(
+            keyName = "toggle",
+            name = "Toggle",
+            description = "",
+            position = -2
+    )
+    default Keybind toggle() {
+        return Keybind.NOT_SET;
+    }
+
+    @ConfigItem(
+            name = "Heal HP",
+            keyName = "heal",
+            description = "Set 0 to disable healing or it will heal at config HP",
+            position = 0
+    )
+    default int heal() {
+        return 0;
+    }
+
+    @ConfigItem(
+            name = "Strength pot?",
+            keyName = "strpot",
+            description = "Set 0 to disable healing or it will heal at config HP",
+            position = 0
+    )
+    default boolean strpot() {
+        return false;
+    }
+}
---
+++ EfMonkKilla/src/main/java/com/ef9/EfMonkKilla/State.java
@@ -0,0 +1,14 @@
+package com.ef9.EfMonkKilla;
+
+public enum State {
+    RUNNING_MONKS,
+    RUNNING_CABBAGES,
+    TALK_MONK,
+    ATTACK_MONK,
+    ATTACKING_MONK,
+    BREAK,
+    WAITING,
+    TIMEOUT,
+    CONTINUE,
+    GET_HEAL
+}
